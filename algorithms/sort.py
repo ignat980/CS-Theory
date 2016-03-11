177f98cef0246d67d08b6933973123e9df4d3d86
Ignat Remizov <ignat980@gmail.com>
2016-03-10 16:23:55 -0800

    Added a sort_by method which lets you sort dictionaries

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -1,14 +1,13 @@
-"""
-Human sort: (Let's say we are sorting a deck of cards)
-1. Find the lowest card O(n)
-2. Put it in a new stack O(1)
-3. Find the next highest card o(n)
-4. Put it in front of the previous sorted card O(1)
-5. Repeat 3 and 4 until you have all the cards sorted in the new set O(n^2)
-"""
-
-
 def selection_sort(array):
+    """Your standard selection sort, as used by humans
+
+    Find the lowest value O(n)
+    Put it in a new array O(1)
+    Find the next highest value O(n)
+    Put it in front of the previous sorted card O(1)
+    Repeat 3 and 4 until you have all the values sorted
+    O(n^2)
+    """
     copy = array[:]
     sorted = []
     size = len(array)
@@ -21,40 +20,12 @@ def selection_sort(array):
         del copy[smallest_index]
     return sorted
 
-# [10, 6, 18, 13, 16, 0, 7, 3, 11, 15]
-# [10] [6, 18, 13, 16, 0, 7, 3, 11, 15] max:10 min:10
-# 1st
-# [6, 10] [18, 13, 16, 0, 7, 3, 11, 15] max:10 min:6
-# < max
-# [6, 10, 18] [13, 16, 0, 7, 3, 11, 15] max:18 min:6
-# > max
-# [6, 10, 13, 18] [16, 0, 7, 3, 11, 15] max:18 min:6
-#     ^
-# % = 58% , idx = 1.75, sort >
-# [6, 10, 13, 16, 18] [0, 7, 3, 11, 15] max:18 min:6
-#                 ^
-# % = 83.3% , idx = 3.33, sort <
-# [0, 6, 10, 13, 16, 18] [7, 3, 11, 15] max:18 min:0
-# < min
-# [0, 6, 7, 10, 13, 16, 18] [3, 11, 15] max:18 min:0
-#           ^
-# % = 38.8% , idx = 2.33, sort <
-# [0, 3, 6, 7, 10, 13, 16, 18] [11, 15] max:18 min:0
-#        ^
-# % = 16.6% , idx = 1.16, sort <
-# [0, 3, 6, 7, 10, 11, 13, 16, 18] [15] max:18 min:0
-#              ^
-# % = 61.1% , idx = 4.8, sort >
-# [0, 3, 6, 7, 10, 11, 13, 15, 16, 18] max:18 min:0
-#                       ^
-# % = 83.3% , idx = 7.5, sort >
-
 
 def insertion_sort(array):
     """A bit faster insertion sort
 
     This variation decides where the index should be before inserting.
-    Ω(n), O(n^2)
+    O(n^2)
     """
     if not array:
         return []
@@ -91,3 +62,23 @@ def insertion_sort(array):
             else:
                 sort.insert(idx, val)
     return sort
+from enum import Enum
+
+
+class Order(Enum):
+    asc = 1
+    desc = 0
+
+
+def sort_by(dicts, prop, order=Order.asc):
+    vals = []
+    returned = []
+    for idx, dict in enumerate(dicts):
+        vals.append((dict[prop], idx))
+    vals.sort()
+    for _, idx in vals:
+        returned.append(dicts[idx])
+    if order is Order.asc:
+        return returned
+    else:
+        return list(reversed(returned))
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -22,3 +22,14 @@ class TestSort(unittest.TestCase):
         self.assertListEqual(selection_sort([23, 42, 4, 16, 8, 15]), [4, 8, 15, 16, 23, 42])
         self.assertListEqual(selection_sort([-2, 1, 3, 18, 20, -28, 0]), [-28, -2, 0, 1, 3, 18, 20])
         self.assertListEqual(selection_sort([19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+
+    def test_sort_by(self):
+        data = [
+            {'origin': 'SFO', 'dest': 'JFK', 'duration': 5.5, 'price': '$249', 'airline': 'JetBlue'},
+            {'origin': 'SFO', 'dest': 'LGA', 'duration': 7.75, 'price': '$201', 'airline': 'Spirit'},
+            {'origin': 'OAK', 'dest': 'LGA', 'duration': 5.83, 'price': '$225', 'airline': 'Delta'},
+            {'origin': 'OAK', 'dest': 'NWK', 'duration': 8.13, 'price': '$189', 'airline': 'United'},
+            {'origin': 'OAK', 'dest': 'JFK', 'duration': 5.32, 'price': '$244', 'airline': 'Virgin America'}
+        ]
+        self.assertEqual(sort_by(data, 'duration')[0]['duration'], 5.32)
+        self.assertEqual(sort_by(data, 'price')[0]['price'], '$189')
